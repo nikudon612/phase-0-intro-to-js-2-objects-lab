908266895830682edcdc0ef28b56702d047f7a70
Nick Bechtel <bechtel.nick@gmail.com>
2021-09-29 15:15:45 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: "Niku",
+    streetAddress: "866 Woodward Ave",
+};
+
+function updateEmployeeWithKeyAndValue() {
+    const newEmployee = {...employee};
+
+    newEmployee ["name"] = "Sam";
+    newEmployee ["streetAddress"] = "11 Broadway";
+
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee ["streetAddress"] = "12 Broadway";
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey() {
+    const newEmployee = {...employee};
+
+    delete newEmployee["name"];
+
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee ["name"];
+
+    return employee;
+}
